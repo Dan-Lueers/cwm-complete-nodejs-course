7f97bdc44763eb568ba602eed49181718dce6c50
Dan-Lueers <dan.lueers@gmail.com>
2025-06-30 13:36:51 +0200

    promise api

---
+++ async-demo/promise-api.js
@@ -0,0 +1,6 @@
+// const p = Promise.resolve({ id: 1 });
+const p = Promise.reject(new Error("reason for rejection..."));
+p.catch((error) => console.log(error));
+// p.then((result) => console.log(result));
+
+// Use a native error object because it includes the call stack
